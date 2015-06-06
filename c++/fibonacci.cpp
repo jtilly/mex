0a408ac06260d30d27d837e9c6a44786ae07e240
Jan Tilly <jantilly@gmail.com>
2015-06-05 22:40:18 -0400

    formatting

--- c++/fibonacci.cpp
+++ c++/fibonacci.cpp
@@ -1,12 +1,11 @@
 #include "fibonacci.h"
 
 int fib(const int n) {
-	int fnum;
-	if (n<2) {
-       fnum = n;
-	}
-    else {
-       fnum = fib(n-1) + fib(n-2);
-    }
-	return fnum;
+  int fnum;
+  if (n<2) {
+    fnum = n;
+  } else {
+    fnum = fib(n-1) + fib(n-2);
+  }
+  return fnum;
 }
