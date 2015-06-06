e58a41301a29720ea00a999b1e27f68cb00ab4b0
Jan Tilly <jantilly@gmail.com>
2015-06-05 22:39:04 -0400

    formatting

--- fortran/globaldef.f90
+++ fortran/globaldef.f90
@@ -1,9 +1,9 @@
 module globaldef
     
-    implicit none
-    public
+  implicit none
+  public
 
-    ! Define precision type 
-    integer,  parameter :: dp = kind(1.0D0)
+  ! Define precision type 
+  integer,  parameter :: dp = kind(1.0D0)
 
 end module globaldef
